d78d2c9b81f2050c9c5ebcf87f7ac7bb0ebba9bc
camilly <cacavh556@gmail.com>
2019-07-19 15:05:57 -0300

    adicionar condicoes

---
+++ script.js
@@ -0,0 +1,23 @@
+// let idade = Number(prompt("Digite sua idade"));
+// let salario = Number(prompt("Digite o seu salário mensal"));
+// let emprestimo = Number(prompt("Digite o valor do empréstimo que você quer adquirir "));
+// let limite = salario * 10;
+
+
+// if (idade >= 25 && idade <= 60) {
+//     if (salario >= 2000){
+//         if (emprestimo < limite){
+//             console.log("emprestimo aprovado")
+//         } else {
+//             console.log("o valor solicitado não pode ser maior que 10x o salário")
+//         }
+//     } else {
+//         console.log("salario recusado")
+//     }
+// }
+// else {
+//     console.log("emprestimo recusado: idade inválida")
+// }
+
+
+
